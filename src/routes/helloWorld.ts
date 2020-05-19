b342a0607149e916f2b3cf502ee0ad91e0050556
Nicolas Lma <teixlima@gmail.com>
2020-05-19 15:36:24 -0300

    basic structure done with example test

--- src/routes/helloWorld.ts
+++ src/routes/helloWorld.ts
@@ -2,6 +2,6 @@ import { Request, Response, Router } from 'express';
 
 const helloWorld: Router = Router();
 
-helloWorld.get('/hi', (req: Request, res: Response) => res.send('Hello World!'));
+helloWorld.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 
 export default helloWorld;
---
+++ src/tests/helloWorld.test.ts
@@ -0,0 +1,9 @@
+import supertest from 'supertest';
+import app from '../app';
+
+describe('Should test application basic structure by testing get helloWorld route', () => {
+    it('should return response.status = 200', async () => {
+        const response = await supertest(app).get('/helloWorld/');
+        expect(response.status).toBe(200);
+    });
+});
