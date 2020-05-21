2c26583e0bbedcd0d135217cdf19be0c38c72027
Nicolas Lma <teixlima@gmail.com>
2020-05-21 07:13:32 -0300

    endpoint working

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import crawler from './crawler';
 import helloWorld from './helloWorld';
 
 const mainRouter: Router = Router();
 
 mainRouter.use('/helloWorld', helloWorld);
+mainRouter.use('/crawler', crawler);
+
 
 export default mainRouter;
--- src/tests/crawler.test.ts
+++ src/tests/crawler.test.ts
@@ -1,4 +1,6 @@
-import getMercadoLivreData from '../utils/crawler'
+import supertest from 'supertest';
+import app from '../app';
+import getMercadoLivreData from '../utils/crawler';
 
 describe('should test crawler', () => {
     it('should get list of galaxy -- success', async () => {
@@ -8,6 +10,16 @@ describe('should test crawler', () => {
         expect(results[0].link).not.toBeUndefined();
         expect(results.length).toBeGreaterThan(0);
     });
+    it('should get list of galaxy -- success -- request', () => {
+        supertest(app)
+            .post('/crawler/products')
+            .send({
+                'limit': 5,
+                'search': 'galaxy'
+            })
+            .expect('Content-Type', /json/)
+            .expect(200);
+    });
     it('should get list of galaxy -- success -- passing limit', async () => {
         const results = await getMercadoLivreData('galaxy', 2);
         expect(results[0].name).not.toBeUndefined();
@@ -15,12 +27,22 @@ describe('should test crawler', () => {
         expect(results[0].link).not.toBeUndefined();
         expect(results.length).toBe(2);
     });
-    it('should throw error -- empty result', async () => {
+    it('should throw error -- empty -- failure', async () => {
         try {
             await getMercadoLivreData('oksgytdp');
         } catch (err) {
             expect(err.message).toBe('NO_RESULTS_FOUND');
         }
         expect.assertions(1);
+    });
+    it('should get list of galaxy -- failure -- request', () => {
+        supertest(app)
+            .post('/crawler/products')
+            .send({
+                'limit': 5,
+                'search': 'galaxy'
+            })
+            .expect('Content-Type', /json/)
+            .expect(400);
     });
 });
