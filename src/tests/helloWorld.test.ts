a4a5f792a2276e56fd10f1f95a7d5fb035de23d8
Nicolas Lma <teixlima@gmail.com>
2020-05-21 04:59:12 -0300

    testing ci/cd

--- src/tests/helloWorld.test.ts
+++ src/tests/helloWorld.test.ts
@@ -2,7 +2,7 @@ import supertest from 'supertest';
 import app from '../app';
 
 describe('Should test application basic structure by testing get helloWorld route', () => {
-    it('should return response.status = 200', async () => {
+    it('should return response.status = 200 -- get', async () => {
         const response = await supertest(app).get('/helloWorld/');
         expect(response.status).toBe(200);
     });
