bb0610570a06e220b2bf010f245fcdd980b0df92
Nicolas Lma <teixlima@gmail.com>
2020-05-22 14:45:44 -0300

    trim applied to response

--- src/utils/crawler.ts
+++ src/utils/crawler.ts
@@ -10,16 +10,16 @@ const getMercadoLivreData = async (search: string, limit: number = 10): Promise<
 
         $('.item__info-title').each(function (i) {
             if (!firstPageData[i] && i < limit) firstPageData[i] = { name: '', store: '', price: '', link: '' };
-            i < limit ? firstPageData[i].name = $(this).text() : null;
+            i < limit ? firstPageData[i].name = $(this).text().trim() : null;
         });
         $('.price__fraction').each(function (i) {
-            i < limit ? firstPageData[i].price = $(this).text() : null;
+            i < limit ? firstPageData[i].price = $(this).text().trim() : null;
         });
         $('.item__brand-title-tos').each(function (i) {
-            i < limit ? firstPageData[i].store = $(this).text().slice(4) : null;
+            i < limit ? firstPageData[i].store = $(this).text().slice(4).trim() : null;
         });
         $('.image-content').each(function (i) {
-            i < limit ? firstPageData[i].link = $(this).find('a').attr('href') : null;
+            i < limit ? firstPageData[i].link = $(this).find('a').attr('href').trim() : null;
         });
 
         return firstPageData;
